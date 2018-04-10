a95c2d28e9f280fd8297509f65c20f959d35e265
Jiang Fengming <fenivana@noindoin.com>
2018-04-10 21:13:32 +0800

    fix: allChunks should be generated every compilation

--- HtmlWebpackIncludeSiblingChunksPlugin.js
+++ HtmlWebpackIncludeSiblingChunksPlugin.js
@@ -1,29 +1,25 @@
 class HtmlWebpackIncludeSiblingChunksPlugin {
   apply(compiler) {
-    let allChunks
+    compiler.hooks.emit.tap('HtmlWebpackIncludeSiblingChunksPlugin', compilation => {
+      const chunkOnlyConfig = {
+        assets: false,
+        cached: false,
+        children: false,
+        chunks: true,
+        chunkModules: false,
+        chunkOrigins: false,
+        errorDetails: false,
+        hash: false,
+        modules: false,
+        reasons: false,
+        source: false,
+        timings: false,
+        version: false
+      }
 
-    compiler.hooks.compilation.tap('HtmlWebpackIncludeSiblingChunksPlugin', compilation => {
+      const allChunks = compilation.getStats().toJson(chunkOnlyConfig).chunks
+
       compilation.hooks.htmlWebpackPluginAlterChunks.tap('HtmlWebpackIncludeSiblingChunksPlugin', chunks => {
-        if (!allChunks) {
-          const chunkOnlyConfig = {
-            assets: false,
-            cached: false,
-            children: false,
-            chunks: true,
-            chunkModules: false,
-            chunkOrigins: false,
-            errorDetails: false,
-            hash: false,
-            modules: false,
-            reasons: false,
-            source: false,
-            timings: false,
-            version: false
-          }
-
-          allChunks = compilation.getStats().toJson(chunkOnlyConfig).chunks
-        }
-
         const ids = [].concat(...chunks.map(chunk => [chunk.id, ...chunk.siblings]))
         return allChunks.filter(chunk => ids.includes(chunk.id))
       })
